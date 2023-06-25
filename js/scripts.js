577eca28c66ec823eae1f42cfce02cc3506236c9
Matteo <Matteoshurdhaqi@gmail.com>
2023-06-25 19:47:29 +0200

    card dinamiche che mostrano contenuto, autore e immagine copertina libri cercati, da introdurre form di ricerca e navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
 "use strict";
 function getBooks(booktitle) {
+    const { from } = rxjs;
+    const { map, switchMap, tap } = rxjs.operators;
     let apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
-    const p = fetch(apiurl + booktitle).then(res => res.json())
-        .then(books => console.log(books));
+    const p = fetch(apiurl + booktitle).then(res => res.json());
+    // .then(books => console.log(books))
+    from(p).pipe(switchMap((data) => from(data.items)), map((ele) => {
+        const book = {
+            title: ele.volumeInfo.title,
+            categories: ele.volumeInfo.categories,
+            authors: ele.volumeInfo.authors,
+            description: ele.volumeInfo.description,
+            thumbnail: ele.volumeInfo.imageLinks.thumbnail
+        };
+        return book;
+    }))
+        .subscribe((book) => displayBook(book));
+    //console.log(data.items));
 }
-getBooks('java');
+function displayBook(book) {
+    const bookTpl = `
+    <div class="card" style="width: 18rem;">
+    <img src="${book.thumbnail}" alt="${book.title}">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">${book.title}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+`;
+    const div = document.createElement('div');
+    div.setAttribute('class', 'col-md-3 py-3');
+    div.innerHTML = bookTpl;
+    document.querySelector('#books').appendChild(div);
+}
+getBooks('Manzoni');
--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,85 @@
+
+
+declare const rxjs: any;
+
+interface GoogleBook{
+    totalItems: number
+    kind: string
+    items: []
+}
+
+
+
+interface BookThumbnails{
+    smallThumbnail: string
+    thumbnail: string
+}
+interface VolumeInfo{
+    thumbnails: string;
+    authors: []
+    description: string
+    imageLinks: BookThumbnails
+    infoLink: string
+    language: string
+    previewLink: string
+    title: string
+     categories: []
+}
+interface Book{
+    title: string 
+    description: string
+    authors: []
+    categories:[]
+    thumbnail : string
+}
+interface BookItem{
+    volumeInfo: VolumeInfo
+    id: string
+}
+
+
 function getBooks(booktitle: string) {
+    const {from} = rxjs;
+    const {map, switchMap, tap} = rxjs.operators;
     let apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-    const p = fetch(apiurl + booktitle).then(res => res.json())
-    .then(books => console.log(books))   
+    const p = fetch(apiurl + booktitle).then(res => res.json());
+    // .then(books => console.log(books))
+    from(p).pipe(
+        switchMap((data:GoogleBook) => from(data.items)),
+        
+        map((ele:BookItem)=>{
+            const book:Book = {
+                title : ele.volumeInfo.title,
+                categories: ele.volumeInfo.categories,
+                authors: ele.volumeInfo.authors,
+                description: ele.volumeInfo.description,
+                thumbnail: ele.volumeInfo.imageLinks.thumbnail
+            };
+            return book;
+        }
+        ),
+        //tap( (book:Book) => console.log(book)),
+    )
+    .subscribe( (book:Book) => displayBook(book))
+    //console.log(data.items));
 }
-getBooks('java');
+function displayBook(book : Book){
+    const bookTpl = `
+    <div class="card" style="width: 18rem;">
+    <img src="${book.thumbnail}" alt="${book.title}">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">${book.title}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+`;
+const div = document.createElement('div');
+div.setAttribute('class','col-md-4 py-3');
+div.innerHTML = bookTpl;
+document.querySelector('#books').appendChild(div)
+
+}
+getBooks('google');
 
