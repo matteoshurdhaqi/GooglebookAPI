dcddfb7b1a49dca4b003976f4ca656002692426f
Matteo <Matteoshurdhaqi@gmail.com>
2023-06-27 00:01:49 +0200

    App ricerca libri completata, da rivedere front-end

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ function getBooks(booktitle) {
     let apiurl = 'https://www.googleapis.com/books/v1/volumes?q=';
     const p = fetch(apiurl + booktitle).then(res => res.json());
     // .then(books => console.log(books))
-    from(p).pipe(switchMap((data) => from(data.items)), map((ele) => {
+    return from(p)
+        .pipe(tap((data) => showTotal(data.items.length)), switchMap((data) => from(data.items || [])), map((ele) => {
         const book = {
             title: ele.volumeInfo.title,
             categories: ele.volumeInfo.categories,
@@ -14,24 +15,54 @@ function getBooks(booktitle) {
             thumbnail: ele.volumeInfo.imageLinks.thumbnail
         };
         return book;
-    }))
-        .subscribe((book) => displayBook(book));
-    //console.log(data.items));
+    }));
+    function showTotal(total) {
+        const found = document.querySelector('#found');
+        if (found) {
+            found.textContent = '' + total;
+        }
+    }
 }
 function displayBook(book) {
     const bookTpl = `
-    <div class="card" style="width: 18rem;">
-    <img src="${book.thumbnail}" alt="${book.title}">
-        <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.title}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card" style="width: 15rem;">
+            <img class="img-fluid" src="${book.thumbnail}" alt="${book.title}" style="max-height:15rem">
+        <div class="px-5"></div>
+            <h5>${book.title}</h5>
+            <p class="py-2">${book.description || ''}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+        
     </div>
 `;
     const div = document.createElement('div');
-    div.setAttribute('class', 'col-md-3 py-3');
+    div.setAttribute('class', 'col-md-3');
     div.innerHTML = bookTpl;
-    document.querySelector('#books').appendChild(div);
+    const books = document.querySelector('#books');
+    if (books) {
+        books.appendChild(div);
+    }
 }
-getBooks('Manzoni');
+function cleanBooks() {
+    const books = document.querySelector('#books');
+    if (books) {
+        books.innerHTML = '';
+    }
+}
+function searchBooks() {
+    const searchEle = document.querySelector('#search');
+    const { fromEvent, debounceTime } = rxjs;
+    const { filter, map, switchMap, tap } = rxjs.operators;
+    if (searchEle) {
+        fromEvent(searchEle, 'keyup')
+            .pipe(map((ele) => ele.target.value), filter((ele) => ele.length > 2), debounceTime(1200), tap(() => cleanBooks()), switchMap((ele) => getBooks(ele))).subscribe((book) => displayBook(book));
+        //getBooks('php');
+    }
+}
+searchBooks();
+function searchButtonClick() {
+    const books = document.querySelector('#search');
+    if (books) {
+        getBooks(books.value).subscribe((book) => displayBook(book));
+    }
+}
--- ts/index.ts
+++ ts/index.ts
@@ -45,8 +45,10 @@ function getBooks(booktitle: string) {
 
     const p = fetch(apiurl + booktitle).then(res => res.json());
     // .then(books => console.log(books))
-    from(p).pipe(
-        switchMap((data:GoogleBook) => from(data.items)),
+    return from(p)
+    .pipe(
+        tap((data:GoogleBook) => showTotal(data.items.length)),
+        switchMap((data:GoogleBook) => from(data.items || [])),
         
         map((ele:BookItem)=>{
             const book:Book = {
@@ -61,25 +63,63 @@ function getBooks(booktitle: string) {
         ),
         //tap( (book:Book) => console.log(book)),
     )
-    .subscribe( (book:Book) => displayBook(book))
-    //console.log(data.items));
+    function showTotal(total : number){
+        const found = document.querySelector('#found');
+        if(found){
+            found.textContent = ''+total;
+        }
+    }
 }
 function displayBook(book : Book){
     const bookTpl = `
-    <div class="card" style="width: 18rem;">
-    <img src="${book.thumbnail}" alt="${book.title}">
-        <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.title}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card" style="width: 15rem;">
+            <img class="img-fluid" src="${book.thumbnail}" alt="${book.title}" style="max-height:15rem">
+        <div class="px-5"></div>
+            <h5>${book.title}</h5>
+            <p class="py-2">${book.description || ''}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+        
     </div>
 `;
 const div = document.createElement('div');
-div.setAttribute('class','col-md-4 py-3');
+div.setAttribute('class','col-md-3');
 div.innerHTML = bookTpl;
-document.querySelector('#books').appendChild(div)
+const books = document.querySelector('#books');
+if(books){
+    books.appendChild(div);
+}
 
 }
-getBooks('google');
+function cleanBooks(){
+    const books = document.querySelector('#books');
+    if(books){
+        books.innerHTML = '';
+    }
+}
+function searchBooks(){
+const searchEle = document.querySelector('#search');
+const {fromEvent, debounceTime} = rxjs;
+const{filter, map, switchMap, tap} = rxjs.operators;
+if(searchEle){
+    fromEvent(searchEle, 'keyup')
+        .pipe(
+            map((ele:any) => ele.target.value),
+            filter( (ele: string) => ele.length >2),
+            debounceTime(1200),
+            tap(() => cleanBooks()),
+            switchMap((ele: string) => getBooks(ele) )
+        ).subscribe((book:Book) => displayBook(book))
+    //getBooks('php');
+}
+
+}
+searchBooks();
+function searchButtonClick(){
+    const books:any = document.querySelector('#search');
+    if(books){
+        getBooks(books.value).subscribe((book : Book)=>displayBook(book))
+    }
+}
+
 
